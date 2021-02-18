1f101a5018fbdd56fbb6067492d8ab1561f6a44f
RogueNPC <jordanbtansay@gmail.com>
2021-02-18 13:40:13 -0800

    [ADD] Created Authentication Forms

--- grocery_app/forms.py
+++ grocery_app/forms.py
@@ -1,8 +1,8 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, FloatField, DateField, SelectField, SubmitField
+from wtforms import StringField, FloatField, DateField, SelectField, SubmitField, PasswordField
 from wtforms.ext.sqlalchemy.fields import QuerySelectField
-from wtforms.validators import DataRequired, InputRequired, Length, NumberRange, URL
-from grocery_app.models import ItemCategory, GroceryStore, GroceryItem
+from wtforms.validators import DataRequired, InputRequired, Length, NumberRange, URL, ValidationError
+from grocery_app.models import ItemCategory, GroceryStore, GroceryItem, User
 
 class GroceryStoreForm(FlaskForm):
     """Form for adding/updating a GroceryStore."""
@@ -29,3 +29,20 @@ class GroceryItemForm(FlaskForm):
     # - photo_url - StringField (use a URL validator)
     # - store - QuerySelectField (specify the `query_factory` param)
     # - submit button
+
+class SignUpForm(FlaskForm):
+    username = StringField('User Name',
+        validators=[DataRequired(), Length(min=3, max=50)])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Sign Up')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('That username is taken. Please choose a different one.')
+
+class LoginForm(FlaskForm):
+    username = StringField('User Name',
+        validators=[DataRequired(), Length(min=3, max=50)])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Log In')
